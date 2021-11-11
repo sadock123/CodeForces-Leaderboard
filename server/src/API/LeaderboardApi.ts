719cbbc8da0ba029fdb9d98e71edf67946f5f6c3
“Samriddha <200020040@iitdh.ac.in>
2021-11-11 14:07:26 +0530

    Completed the fetchleaderboard function

--- server/src/API/LeaderboardApi.ts
+++ server/src/API/LeaderboardApi.ts
@@ -2,7 +2,25 @@ import fetch from 'node-fetch';
 
 export class LeaderboardAPI {
   static async getMemberRatingsApiCall(query: string): Promise<any> {
-    const result = await fetch(query);
-    return result;
+    try {
+      const result = await fetch(query);
+      const data: any = await result.json();
+      if (result.status === 200) {
+        return {
+          status: true,
+          code: 200,
+          data: data?.result
+        };
+      } else {
+        return {
+          status: false,
+          code: data.status,
+          data: null
+        };
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
