8321bc4557ddd0df7de7ef135f25553bb1fec02b
Atul Singh <59884618+AtulSingh-Emyre@users.noreply.github.com>
2021-11-12 17:34:12 +0530

    removed vulnerabilities

--- server/src/environments/dev.env.ts
+++ server/src/environments/dev.env.ts
@@ -1,5 +1,5 @@
 export const DevEnvironment = {
   db_url:
-    'mongodb+srv://admin123:ADMIN1234@cluster0.tfxzy.mongodb.net/codefoleaderboard?retryWrites=true&w=majority',
+    '',
   jwt_secret: ''
 };
