19f29c12b267dea235a97a98ea263e18d84861f6
sadock123 <200020038@iitdh.ac.in>
2022-10-25 22:19:13 +0530

    updated styles.tsx

--- client/src/components/Footer/styles.tsx
+++ client/src/components/Footer/styles.tsx
@@ -1,7 +1,15 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-
-}));
+const useStyles = makeStyles({
+  root: {
+    background: '#007753',
+  },
+  paper: {
+    alignItems: "right",
+  },
+});
 
 export default useStyles;
